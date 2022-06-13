bcb3e2b1a5dfdb058f0c5f37be0251bbd5a6fe3a
EC2 Default User <ec2-user@ip-172-31-10-113.ap-northeast-1.compute.internal>
2022-06-13 19:15:03 +0000

    [会員関連Finish]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,7 +14,7 @@ class Public::CustomersController < ApplicationController
 
 private
   def customer_params
-    params.require(:cusromer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :password, :postal_code, :address, :telephone_number)
+    params.require(:cusromer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,23 @@ Rails.application.routes.draw do
   get '/admin' => 'admin/homes#top', as: 'admin'
   get '/admin/genres' => 'admin/genres#index'
   post '/admin/genres' => 'admin/genres#create'
+  get '/admin/genres/:id/edit' => 'admin/genres#edit', as: 'genre_edit'
+  patch '/admin/genres/:id' => 'admin/genres#update', as: 'genre_update'
+
   get '/admin/items' => 'admin/items#index'
   get '/admin/items/new' => 'admin/items#new'
   get '/admin/items/:id' => 'admin/items#show', as: 'admin_item'
   post '/admin/item_infomations' => 'admin/items#create'
+  get '/admin/items/:id/edit' => 'admin/items#edit', as: 'item_edit'
+  patch '/admin/items/:id' => 'admin/items#update', as: 'item_update'
+
+
+  get '/admin/customers' => 'admin/customers#index'
+  get '/admin/customers/:id' => 'admin/customers#show', as: 'admin_customer'
+  get '/admin/customers/:id/edit' => 'admin/customers#edit', as: 'ad_customer_edit'
+  patch '/admin/customers/:id' => 'admin/customers#update', as: 'ad_customer_update'
+  
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
